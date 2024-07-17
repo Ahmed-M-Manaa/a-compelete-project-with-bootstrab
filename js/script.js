720a2ee7493d9b28c6f3e371dc29c8faa81bddb8
Ahmed-M-Manaa <ahmedmna32005@gmail.com>
2024-07-17 14:53:19 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,159 @@
+// for(i=0 ; i<=100 ; i+=2){
+//     console.log(i)
+// }
+
+
+
+
+
+//  let n=5;
+
+//  for (i=1 ; i<=12 ; i++){
+//     console.log( i + "*"+n+ "="+ n*i );
+//  }
+
+ 
+
+
+
+// const cm =100;
+// var i = 5;
+
+// function  convert() {
+//     sum = i/cm
+//     console.log(sum);
+// }
+// convert()
+
+
+
+
+
+
+// var sum =0;
+// var a= [5,9,4,25,889,6]
+// for (let i = 0 ; i< a.length ; i++){
+//     sum+=a[i]
+// }
+// console.log(sum);
+
+
+
+
+
+
+// var a =  -7
+
+// function  filter() {
+//     if (a > 0) {
+//         console.log("dah rakam positive yasta");  
+//                }
+
+//     else (a < 0)
+//         console.log("NEGA tive");
+//     }
+
+// filter()
+
+
+
+
+// var s = "      saoijfijs      "
+// var r = s.trim()
+// console.log(r);
+
+
+
+
+// var a =  12
+// var a =  9
+// var a =  0
+
+
+// function  filter() {
+//     if (a%2 == 0 ) {
+  
+//         a++
+//         console.log(a);  
+//                }
+
+//     else if (a%2!==0){
+
+//         console.log(a);
+//     }
+//         else (a == 0) 
+//         console.log(a);
+//     }
+
+// filter()
+
+
+
+
+
+// var arr = prompt("enter some numbers or letters seberated by comma ',':")
+// var arr2 = arr.split(",")
+
+// function ret(){
+
+//     var convert = arr2.join("")
+//     console.log(convert);
+
+// }
+// ret()
+
+
+
+// var arr = prompt("enter some numbers:")
+// var sum =0 
+
+// function check() {
+
+//  for ( let i = 0 ; i < arr.length ; i++ ){
+//     sum+=arr[i]
+//  }
+//  if (sum % 2==0 ||sum == "" ) {
+//     console.log("even");
+//  } else
+
+//  console.log("odd");
+
+// }
+
+// check()
+
+
+
+// var fn = prompt("enter the first numbers:")
+// var sn = prompt("enter socund numbers:")
+// var sum  ;
+// var divi ;
+// var multi ;
+
+// function calc() {
+    
+
+// var sum = +fn+ +sn ;
+
+//     console.log( "the sum of the tow numbers is :"+fn+"+"+sn +"=" +sum);
+
+
+// var divi = fn / sn ;
+
+// console.log( "the division of the tow numbers is :"+fn+"/"+sn +"=" +divi);
+
+
+// var multi = fn * sn ;
+
+// console.log( "the multiplication of the tow numbers is :"+fn+"*"+sn +"=" + multi);
+
+
+
+// }
+
+// calc()
+   
+var x = 10 + 5
+
+console.log(typeof x)
+
